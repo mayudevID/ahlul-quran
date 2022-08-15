c9af6e9af2fa144ea9e65529bc5a71f10f3eb36f
maulana2468 <ymaulana089@gmail.com>
2022-08-15 22:11:24 +0700

    update clipper file

--- lib/features/quran/presentation/pages/curve_clipper.dart
+++ lib/features/quran/presentation/pages/curve_clipper.dart
@@ -1,5 +1,4 @@
-import 'package:alquran_mobile_apps/core/utils/function.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class CurveClipper extends CustomClipper<Path> {
   @override
